28fec39357f95d10ee4e7d6f891f585cf39cc433
arkaslittlemind <arkadiptamojumder14@gmail.com>
2021-11-23 19:16:58 +0530

    Semantical Errors resolved

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Get all the Info regarding your favorite Crypto Currency
+            Get all the Info regarding your favorite Cryptocurrency
           </Typography>
         </div>
         <Carousel />
